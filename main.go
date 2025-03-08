b8dc4fbd1275e00f7010542b349351923d2e54c7
keithjdanielson <keithjdanielson@gmail.com>
2025-03-07 17:55:33 -0700

    Reading wav header from go-audio/wav instead of manual

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/go-audio/wav"
 )
 
 func main() {
@@ -14,11 +16,16 @@ func main() {
 	}
 	defer file.Close()
 
-	// First, we need to read the header of the file.
-	headerBuffer := make([]byte, 44)
-	n1, err := file.Read(headerBuffer)
-	if err != nil {
-		log.Fatal(err)
+	decoder := wav.NewDecoder(file)
+	if !decoder.IsValidFile() {
+		fmt.Println("Invalid WAV file")
+		return
 	}
-	fmt.Printf("%d bytes: %s\n", n1, string(headerBuffer[:n1]))
+
+	fmt.Println("WAV Header Information:")
+	fmt.Printf("Audio Format: %d (1 = PCM)\n", decoder.WavAudioFormat)
+	fmt.Printf("Number of Channels: %d\n", decoder.NumChans)
+	fmt.Printf("Sample Rate: %d Hz\n", decoder.SampleRate)
+	fmt.Printf("Bits Per Sample: %d-bit\n", decoder.BitDepth)
+	fmt.Printf("Average Bytes Per Second: %d\n", decoder.AvgBytesPerSec)
 }
