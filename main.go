4a18cbc274213221d0cc901f19e4b71111e5727a
keithjdanielson <keithjdanielson@gmail.com>
2025-03-07 17:45:35 -0700

    Read the file header

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Print("Hello World...")
+	// Open our file
+	file, err := os.Open("tmp/song.wav")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// First, we need to read the header of the file.
+	headerBuffer := make([]byte, 44)
+	n1, err := file.Read(headerBuffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d bytes: %s\n", n1, string(headerBuffer[:n1]))
 }
